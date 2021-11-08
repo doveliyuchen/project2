5c49a9e7719bea7740f0f359d415ad9b141ab80f
Dove Li <54865789+doveliyuchen@users.noreply.github.com>
2021-11-09 02:22:08 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,43 +1,88 @@
-//Initialize the express 'app' object
+//STEP 2.
+//Express
 let express = require('express');
 let app = express();
 app.use('/', express.static('public'));
 
-//Initialize the actual HTTP server
+//Server
 let http = require('http');
 let server = http.createServer(app);
 let port = process.env.PORT || 3000;
-server.listen(port, ()=> {
-    console.log('listening at ', port);
+server.listen(port, () => {
+  console.log("Server is listening at: " + port);
 });
 
-
-//Initialize socket.io
+//STEP 3. Socket connection
 let io = require('socket.io');
 io = new io.Server(server);
 
-//Listen for individual clients/users to connect
-io.sockets.on('connection', function(socket) {
-    console.log("We have a new client: " + socket.id);
+//Establish default socket connection
+io.sockets.on('connection', (socket) => {
+  console.log("We have a new client: " + socket.id);
 
-    //Listen for a message named 'data' from this client
-    socket.on('data', function(data) {
-        //Data can be numbers, strings, objects
-        console.log("Received: 'data' " + data);
+  //STEP 6. Listen for data
+  socket.on('data', function(data) {
+    //Data can be numbers, strings, objects
+    console.log("Received: 'data' " + data);
 
-        //Send the data to all clients, including this one
-        //Set the name of the message to be 'data'
-        io.sockets.emit('data', data);
+    //Send the data to all clients, including this one
+    //Set the name of the message to be 'data'
+    io.sockets.emit('data', data);
 
-        //Send the data to all other clients, not including this one
-        // socket.broadcast.emit('data', data);
+    //Send the data to all other clients, not including this one
+    // socket.broadcast.emit('data', data);
 
-        //Send the data to just this client
-        // socket.emit('data', data);
-    });
+    //Send the data to just this client
+    // socket.emit('data', data);
+});
 
-    //Listen for this client to disconnect
-    socket.on('disconnect', function() {
-        console.log("A client has disconnected: " + socket.id);
-    });
+  socket.on('disconnect', () =>{
+    console.log('Client disconnected: ' + socket.id);
+  });
+});
+
+//private namespace connection
+let private = io.of('/private');
+private.on('connection', (socket) => {
+  console.log("We have a new client: " + socket.id);
+
+  //listen for room name
+  socket.on('room-name', data => {
+    console.log(data);
+
+    //add socket to room
+    socket.join(data.room);
+
+    //add room property to socket object
+    socket.room = data.room;
+    // console.log(socket);
+
+    //send a welcome message to new clients
+    let welcomeMsg = "Welcome to '" + data.room + "' room!";
+
+    //send a welcome message to everybody in a room
+    // private.to(socket.room).emit('joined', {msg: welcomeMsg});
+    socket.emit('joined', {msg: welcomeMsg});
+  });
+
+  //STEP 6. Listen for data
+  socket.on('data', function(data) {
+    //Data can be numbers, strings, objects
+    console.log("Received: 'data' " + data);
+
+    //Send the data to all clients, including this one
+    //Set the name of the message to be 'data'
+    private.to(socket.room).emit('data', data);
+
+  
+});
+
+
+  socket.on('disconnect', () =>{
+    // console.log('Client disconnected: ' + socket.id);
+    // console.log('Client ' + socket.id + ' disconnected and left room ' + socket.room);
+    console.log(`Client ${socket.id} disconencted and left room ${socket.room}`);
+
+    socket.leave(socket.room);
+  });
 });
