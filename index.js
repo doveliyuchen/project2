96ba1278a1f13e9980b4c4547585953cb7e8c54d
Dove Li <54865789+doveliyuchen@users.noreply.github.com>
2021-11-09 01:56:44 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ app.use('/', express.static('public'));
 let http = require('http');
 let server = http.createServer(app);
 let port = process.env.PORT || 3000;
-server.listen(port, () => {
-    console.log("Server listening at port: " + port);
+server.listen(port, ()=> {
+    console.log('listening at ', port);
 });
+
 
 //Initialize socket.io
 let io = require('socket.io');
